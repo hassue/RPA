78b61e55275db6252623b0b73b467ae075fff6e2
Hassan Ullah <hassue@gmail.com>
2025-07-30 17:34:55 +0500

    CS EXTRACT HTML

---
+++ CS_HTML/ConsoleAppHtmlAgilityPack/ConsoleAppHtmlAgilityPack/Program.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHtmlAgilityPack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load HTML content
+            var html = @"
+            <html>
+                <body>
+                    <div id='main'>
+                        <p class='info'>Hello World</p>
+                        <a href='https://example.com'>Click Here</a>
+                    </div>
+                </body>
+            </html>";
+
+            // Load HTML into HtmlDocument
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            // Example 1: Extract text using XPath
+            var paragraph = htmlDoc.DocumentNode.SelectSingleNode("//p[@class='info']");
+            Console.WriteLine("Paragraph text: " + paragraph.InnerText);
+
+            // Example 2: Extract attribute using XPath
+            var link = htmlDoc.DocumentNode.SelectSingleNode("//a");
+            Console.WriteLine("Link href: " + link.GetAttributeValue("href", ""));
+        }
+    }
+}
